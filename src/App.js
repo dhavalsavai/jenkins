c84db42c523e83b888a7cb8c61841fc40a80163d
dhaval savai <dhavalsavai3@gmail.com>
2024-11-06 19:53:50 +0530

    updated code content

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <div className="message-box">
                 <h1 className="title">This is</h1>
-                <p className="subtitle">Dev Server</p>
+                <p className="subtitle">Dev Server deployment using pipeline</p>
             </div>
         </div>
     );
