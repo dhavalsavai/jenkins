844a6c6ad584cf95757f06aa5d1b4705d464165d
Dhaval Savai <dhavalsavai3@gmail.com>
2024-11-15 21:01:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <div className="message-box">
                 <h1 className="title">This is</h1>
-                <p className="subtitle">Deployment using helm charts with kubernetes</p>
+                <p className="subtitle">Deployment using helm charts with kubernetes via jenkins pipeline</p>
             </div>
         </div>
     );
